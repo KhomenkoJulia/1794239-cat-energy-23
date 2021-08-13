df5afa245d6e74088cef19e2747f9b543f9d4331
July <j9151139558@gmail.com>
2021-08-13 11:59:27 +0300

    Сделать адаптив главной страницы

--- source/js/slider.js
+++
@@ -1,6 +0,0 @@
-let rangeSlider = document.querySelector('.slider__range');
-let sliderItemBefore = document.querySelector('.slider__item--before');
-
-function moveSlider() {
-  sliderItemBefore.style.width = rangeSlider.value + "%";
-}
