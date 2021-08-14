ed697baa25ca6f25f5f607510d54b0efda257f87
July <j9151139558@gmail.com>
2021-08-14 07:57:09 +0300

    Стилизовать мобильное меню

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,12 @@
-let document
+  let navMenu = document.querySelector('.nav-menu');
+  let navToggle = document.querySelector('.nav-menu__toggle');
+
+  navToggle.addEventListener('click', function() {
+    if (navMenu.classList.contains('nav-menu--closed')) {
+    navMenu.classList.remove('nav-menu--closed');
+  navMenu.classList.add('nav-menu--opened');
+    } else {
+    navMenu.classList.add('nav-menu--closed');
+  navMenu.classList.remove('nav-menu--opened');
+    }
+  });
